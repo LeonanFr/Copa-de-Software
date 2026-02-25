93864186467416e9991482411263a7eeb3000eef
Leonan <pedroleonan85@gmail.com>
2026-02-25 20:28:02 -0300

    fix: correção de cors

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -38,7 +38,7 @@ func AuthMiddleware(authSvc *auth.Service) func(http.Handler) http.Handler {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://seudominio.com")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == "OPTIONS" {

29d2b025893b8014ca473d350017a2a994cca040
Leonan <pedroleonan85@gmail.com>
2026-04-09 13:12:28 -0300

    fix: atualizado link permitido de acesso cors

--- internal/http/middleware.go
+++ internal/http/middleware.go
@@ -50,9 +50,10 @@ func corsMiddleware(next http.Handler) http.Handler {
 }
 
 func setCORS(w http.ResponseWriter) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Origin", "https://copadesoftware.onrender.com")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
 }
 
 func OptionsHandler(next http.Handler) http.Handler {
